a7fe83cbffa8473ed776fe2be8f0c2f946a5da24
Paul Evans <evansp2@gmail.com>
2015-09-28 11:10:06 -0400

    updates

--- run-tests-xcodebuild.sh
+++ run-tests-xcodebuild.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 readonly PROJECT_NAME="PEObjc-Commons"
-readonly SDK_VERSION="8.4"
+readonly SDK_VERSION="9.0"
 readonly ARCH="i386"
 export LC_CTYPE=en_US.UTF-8
-
 
 xcodebuild \
 -workspace ${PROJECT_NAME}.xcworkspace \
